3941cf84f460d7f18132d2389a21840cf7e15a18
akhileshtiwari2 <akhilesh.tiwari02@gmail.com>
2023-09-09 11:30:23 +0530

    Node js connect with MongoDB

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+const connectDB = async ()=>{
+    mongoose.connect("mongodb://127.0.0.1:27017/e-comm");
+    const productSchema = new mongoose.Schema({});
+    const product = mongoose.model('product',productSchema);
+    const data = await product.find();
+    console.warn(data);
+}
+connectDB();
 
-app.get('/', (req, res)=>{
-    res.send("App is working")
-})
 
-app.listen(3000)
+app.listen(5000)
