dd8d25dc1af89d7d568b27fe6b7c0fd7290d571a
akhileshtiwari2 <akhilesh.tiwari02@gmail.com>
2023-09-09 19:06:35 +0530

    Login API and hide password in signup API

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-const express = require('express');
-require('./db/config')
-const User = require('./db/users');
+const express = require("express");
+const cors = require("cors");
+require("./db/config");
+const User = require("./db/users");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.post('/register',async(req, res)=>{
-    let user = new User(req.body);
-    let result = await user.save();
-    res.send(result)
-})
-app.listen(5000)
+app.post("/register", async (req, res) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  //Remove Password
+  result = result.toObject();
+  delete result.password;
+  res.send(result);
+});
+
+app.post("/login", async (req, res) => {
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      res.send(user);
+    } else {
+      res.send({ result: "No User Found" });
+    }
+  } else {
+    res.send({ result: "No User Found" });
+  }
+});
+app.listen(5000);
