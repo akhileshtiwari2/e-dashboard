0bd80d7dcceaae87e67ff2235cdfaa326758b0ce
akhileshtiwari2 <akhilesh.tiwari02@gmail.com>
2023-09-10 20:59:08 +0530

    Add Product API

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("./db/config");
 const User = require("./db/users");
+const Product = require("./db/product")
 const app = express();
 
 app.use(express.json());
@@ -28,4 +29,10 @@ app.post("/login", async (req, res) => {
     res.send({ result: "No User Found" });
   }
 });
+
+app.post('/add-product',async(req, res)=>{
+let product = new Product(req.body);
+let result  = await product.save();
+res.send(result)
+})
 app.listen(5000);
