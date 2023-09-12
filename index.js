bc218a9e5b56dc992c53ca11d10ab98320cb7f5f
akhileshtiwari2 <akhilesh.tiwari02@gmail.com>
2023-09-12 15:51:50 +0530

    Update Product API

--- index.js
+++ index.js
@@ -49,4 +49,23 @@ app.delete('/product/:id',async(req, res)=>{
   const result = await Product.deleteOne({_id:req.params.id})
   res.send(result);
 })
+
+app.get('/product/:id',async(req, res)=>{
+  let result = await Product.findOne({_id:req.params.id})
+  if(result){
+    res.send(result)
+  }else{
+    res.send({result:"No record found"})
+  }
+})
+
+app.put("/product/:id",async(req,res)=>{
+  let result = await Product.updateOne(
+    {_id: req.params.id},
+    {
+      $set: req.body
+    }
+    )
+    res.send(result)
+})
 app.listen(5000);
